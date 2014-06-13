4eba56ee6c67f0c7db1ad111d14f17cfc2b7fc20
Zipfian Academy <Zipfian@Zipfians-Mac-mini-8.local>
2014-06-13 16:06:28 -0700

    changes, RandomForrest

--- mjk.py
+++ mjk.py
@@ -11,6 +11,17 @@ from sklearn import metrics
 import json
 from sklearn.linear_model import LogisticRegression
 from sklearn import cross_validation
+from sklearn.ensemble import RandomForestClassifier
+
+def fromJson(a):
+	t = ' '
+	b = ' '
+	j = json.loads(a)
+	if j.get('title'):
+		t = j['title']
+	if j.get('body'):
+		t = j['body']
+	return t + ' ' + b
 
 #df = pd.read_pickle("articles.pkl")
 df = pd.read_csv("data/train.tsv", delimiter='\t')
@@ -21,14 +32,16 @@ u'snippet', u'news_desk', u'word_count', u'source', u'section_name',
 u'subsection_name', u'_id', u'pub_date', u'print_page', u'headline',
 u'content'],
 '''
+snip = df['boilerplate'].apply(fromJson)
+snip = np.array(snip)
 
-snip = np.array(df['boilerplate'])
+#snip = np.array(df['boilerplate'])
 target = np.array(df['label'])
 #print df['content'].head()
 
 # title = json.loads(snip)['title']
 # titlelen = len(title)/3
-snip = json.loads(snip)['title'] + ' ' + json.loads(snip)['body']
+#snip = json.loads(snip)['title'] + ' ' + json.loads(snip)['body']
 
 vectorizer = TfidfVectorizer(max_df=0.5,
                              stop_words='english')
@@ -36,6 +49,11 @@ vectorizer = TfidfVectorizer(max_df=0.5,
 
 X = vectorizer.fit_transform(snip)
 
-clf_lr  = LogisticRegression(C=1, penalty='l1', tol=0.01)
-scores = cross_validation.cross_val_score(clf_lr, X, target, cv=5)
-print "%s -- %s" % (clf_lr.__class__, np.mean(scores))
+model  = LogisticRegression(C=1, penalty='l1', tol=0.01)
+scores = cross_validation.cross_val_score(model, X, target, cv=5)
+print "%s -- %s" % (model.__class__, np.mean(scores))
+
+model = RandomForestClassifier(verbose=10, n_estimators=1, n_jobs=-1, max_features=None)
+#model.fit(X.toarray(), target)
+scores = cross_validation.cross_val_score(model, X, target, cv=5)
+print "%s -- %s" % (model.__class__, np.mean(scores))
