c6621c1fc220aa40ebeeeed7b64382855131203e
MJK <mjk@emmjaykay.org>
2014-06-15 01:04:08 -0700

    bug fixes for run_model.py

--- mjk-lr.py
+++ mjk-lr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+import getopt
 import sys
 import numpy as np
 import pandas as pd
@@ -17,6 +17,14 @@ from sklearn.grid_search import GridSearchCV
 
 from sklearn.svm import SVC
 
+param_grid_lr = [
+        {'C': [ 1,1.5,2,3,4,5], 'penalty': ['l2'] },
+]
+
+param_grid_svc = [
+        {'C': [ 1,1.5,2,3,4,5], 'gamma': ['0', '.1', '1'], 'kernel': ['rbf', 'linear', 'poly'] },
+]
+
 def fromJson(a):
 	t = ' '
 	b = ' '
@@ -29,7 +37,7 @@ def fromJson(a):
 
 #df = pd.read_pickle("articles.pkl")
 df = pd.read_csv("data/train.tsv", delimiter='\t')
-print df.columns
+#print df.columns
 '''
 [u'url', u'urlid', u'boilerplate', u'alchemy_category', 
 u'alchemy_category_score', u'avglinksize', u'commonlinkratio_1', 
@@ -46,40 +54,53 @@ snip = np.array(snip)
 
 target = np.array(df['label'])
 
+
 vectorizer = TfidfVectorizer(stop_words='english')
 
-
-
 X = vectorizer.fit_transform(snip)
-#print X
+print "Vectorization complete"
+
+words = vectorizer.get_feature_names()
+print len(words)
 snip = pd.DataFrame(X.toarray(0))
 for i in ['html_ratio', 'numberOfLinks', 'non_markup_alphanum_characters', 'frameTagRatio', 'avglinksize', 'spelling_errors_ratio', 'linkwordscore', 'commonlinkratio_2', 'parametrizedLinkRatio', 'commonlinkratio_1', 'commonlinkratio_3', 'image_ratio']:
         snip[i] = df[i]
-'''
-snip['numberOfLinks'] = df['numberOfLinks']
-snip['non_markup_alphanum_characters'] = df['non_markup_alphanum_characters']
-snip['frameTagRatio'] = df['frameTagRatio']
-snip['avglinksize'] = df['avglinksize']
-snip['spelling_errors_ratio'] = df['spelling_errors_ratio']
-snip['linkwordscore'] = df ['linkwordscore']
-'''
 
-param_grid_lr = [
-        {'C': [ 1,1.5,2,3,4,5], 'penalty': ['l2'] },
-]
 
-param_grid_svc = [
-        {'C': [ 1,1.5,2,3,4,5], 'gamma': ['0', '.1', '1'], 'kernel': ['rbf', 'linear', 'poly'] },
-]
 
-#model  = LogisticRegression()
-model  = SVC()
+
 #scores = cross_validation.cross_val_score(model, snip, target, cv=5)
 #print "%s -- %s" % (model.__class__, np.mean(scores))
 
-clf = GridSearchCV(model, param_grid_svc, n_jobs=2, verbose=10)
-clf.fit(snip, target)
 
 
+if __name__ == "__main__":
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ls", ["logistic", "svm"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        sys.exit(2)
+    output = None
+    verbose = False
+    model = None
+    param_grid = None
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-l", "--logistic"):
+                model  = LogisticRegression()
+                param_grid = param_grid_lr
+        elif o in ("-s", "--svm"):
+                model  = SVC()
+                param_grid = param_grid_svc
+    else:
+            assert False, "unhandled option"
+            sys.exit()
+    # ...
+
+    clf = GridSearchCV(model, param_grid, n_jobs=1, verbose=10)
+    clf.fit(snip, target)
 
 
